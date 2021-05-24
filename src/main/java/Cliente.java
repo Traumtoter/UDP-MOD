5b252ea54449242a5917bced009796f51d2c3d29
Dreamer-byte <parcestar2021@gmail.com>
2021-05-24 02:10:41 -0600

    Iniciando el control de version del proyecto

---
+++ src/main/java/Cliente.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author suneater
+ */
+import java.net.*;
+import java.io.*;
+public class Cliente {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("\tArrancando la aplicacion Cliente\n");
+        
+        BufferedReader EntradaUser = new BufferedReader(new InputStreamReader(System.in));
+        String NameServer ="localhost";
+        String Message;
+        try
+        {
+            InetAddress IpAd = InetAddress.getByName(NameServer);
+            DatagramSocket ConeccionCliente = new DatagramSocket();
+            String cierre = new String("cierre");
+                
+            while(true)
+            {
+                byte[]DatosEnviar = new byte[1024];
+                byte[]DataRecive = new byte[1024];
+
+                System.out.println("Ingresa el mensage para el servidor");
+                Message = EntradaUser.readLine();
+
+                if(Message.equals(""))Message = "cierre";
+                
+                System.out.println("Cadena a enviar: " + Message);
+                DatosEnviar = Message.getBytes();
+                DatagramPacket EnviarPakete = new DatagramPacket(DatosEnviar,DatosEnviar.length,IpAd,5000);
+
+                ConeccionCliente.send(EnviarPakete);
+
+                DatagramPacket RecivirPakete = new DatagramPacket(DataRecive,DataRecive.length);
+                ConeccionCliente.receive(RecivirPakete);
+
+                String Respuesta = new String(RecivirPakete.getData());
+                
+                
+                if(Respuesta.indexOf(cierre)<0)
+                {
+                    
+                    System.out.println("Respuesta del servidor: "+Respuesta + "\n");
+                }
+                else
+                {
+                    
+                    
+                    System.out.println("Respuesta del servidor: "+Respuesta);
+                    
+                    System.out.println("Cerrando la conccecion\n");
+                    ConeccionCliente.close();
+                    break;
+               
+                }
+                
+                
+               
+            }
+            
+            
+        }
+        catch(Exception e)
+        {
+            System.out.println("Ocurrio un error al tratar de conectar: "+e.getMessage() + "\n");
+        }
+        
+        
+        
+        System.out.println("Adios ....");
+    }
+    
+}
---
+++ src/main/java/ServerUDP.java
@@ -0,0 +1,70 @@
+      /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author suneater
+ */
+import java.net.*;
+public class ServerUDP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        try
+        {
+            System.out.println("\n*****Arrancando el servidor*****\n");
+            DatagramSocket Coneccion = new DatagramSocket(5000);
+            String Cerrar = new String("cierre");
+            
+            while(true)
+            {
+                byte[]Cadenarecivida = new byte[1024];
+                byte[] CadenaEnviar  = new  byte[1024];
+                DatagramPacket PacketRecivido = new DatagramPacket(Cadenarecivida,Cadenarecivida.length);
+                Coneccion.receive(PacketRecivido);
+                
+                String CadenaServer = new String(PacketRecivido.getData());
+                CadenaEnviar = CadenaServer.getBytes();
+                
+               InetAddress Ip = PacketRecivido.getAddress();
+               int puerto = PacketRecivido.getPort();
+               
+               System.out.println("Enviando a: " + PacketRecivido.getAddress());
+               
+               DatagramPacket enviarpaqket = new DatagramPacket(CadenaEnviar,CadenaEnviar.length,
+               Ip,puerto);
+               String Senviada = new String(PacketRecivido.getData());
+               Coneccion.send(enviarpaqket);
+               
+              
+               if(CadenaServer.indexOf("cierre") <0 )
+               {
+                   System.out.println("Enviando... "+Senviada);
+                   
+                }
+               else
+               {
+                   
+                   System.out.println("Se detecto cadena vacia para cerrar la conection");
+                    Coneccion.close();
+                    System.out.println("El servidor a cerrado secion\n");
+                    break;
+               }
+               
+               System.out.println("Hola server Abajo");
+            }
+        }
+        catch(Exception e)
+        {
+            System.out.println("Ocurrio un error: "+e.getMessage());
+        }
+        System.out.println("Adios clientete");
+    }
+    
+}
